9830442035a6d2357af1c0a9e2344b5b07a4a7fc
Kostia Chornobuk <kostiantyn.chornobuk@gmail.com>
2022-11-27 18:33:57 +0200

    create project for task2 with tasks

---
+++ task2/src/main/java/com/chornobuk/App.java
@@ -0,0 +1,28 @@
+package com.chornobuk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    // todo:
+    // []1. create entity for rule break
+    // []2. create entity for statistic
+    // []3. write parser to read one rule's break in XML (can rewrite method from taks 1)
+    // []4. test it
+    // []5. write general method that will parse one file and return collection of elements
+            // using method from step 1
+    // []6. method for reading all files from folder
+    // []7. create method for generating statistic
+    // []8. create method for writing data in file
+    // []9. create main method that will use above methods that will take path to the folder
+    // []10. test method
+    // []11. refactoring
+    // put this path to method from step 3, get collection  map of year-collection
+    // move this 
+}
